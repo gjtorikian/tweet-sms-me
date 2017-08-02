8932cfefa4534b8dc7eebdbafc2415438bd74511
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2017-08-02 18:14:13 -0400

    new attempts

--- app.rb
+++ app.rb
@@ -47,7 +47,7 @@ EM.schedule do
   client = TweetStream::Client.new
 
   client.follow([202668848]) do |tweet|
-    if !tweet.retweet? && !tweet.reply? && !tweet.in_reply_to_screen_name?
+    if tweet.text =~ /^bmoviesd:/
       send_sms("#{tweet.user.screen_name}: #{tweet.text}\n#{tweet.url}")
     end
   end
